a6b5a5fbedca305f93528807d293d9a27c891891
kagr <kevin@goldsteinconsultingservices.com>
2025-09-18 10:56:17 -0400

    removed un-used properties

--- validator/src/main/java/com/kagr/videos/validator/SystemExitShutdownHandler.java
+++ validator/src/main/java/com/kagr/videos/validator/SystemExitShutdownHandler.java
@@ -1,12 +1,13 @@
 package com.kagr.videos.validator;
 
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Data
 public class SystemExitShutdownHandler implements ShutdownHandler {
     public void terminateProcess(int exitCode) {
-        logger.info("Terminating process with exit code: {}", exitCode);
         System.exit(exitCode);
     }
 }
